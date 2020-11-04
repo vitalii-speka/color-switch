ece2b3d1624a5a64846cf22d9750ab93947d6366
Vitalii Speka <spekava@gmail.com>
2020-11-05 01:01:24 +0200

    таймер готов, не могу настроить стили....

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-import '../src/css/styles.css';
+// import '../src/css/styles.css';
+import colors from '/js/colors.js';
 
 const btnStart = document.querySelector('[data-action="start"]');
 const btnStop = document.querySelector('[data-action="stop"]');
@@ -13,7 +14,7 @@ const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548',];
+// const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548',];
 
 
 function onBtnStart() {
